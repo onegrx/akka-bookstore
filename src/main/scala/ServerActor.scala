929a86888edba95a20bc897438b9cfa20e78b8cd
onegrx <onegrx@gmail.com>
2017-05-25 21:06:08 +0200

    Create actors on demand

--- src/main/scala/ServerActor.scala
+++ src/main/scala/ServerActor.scala
@@ -9,14 +9,15 @@ class ServerActor extends Actor {
   val selection = context.actorSelection(path)
 
   val internal = ActorSystem("internal")
-  val sw1 = internal.actorOf(Props(SearchActor(1)), name = "searchWorker1")
-  val sw2 = internal.actorOf(Props(SearchActor(2)), name = "searchWorker2")
+
 
   override def receive: Receive = {
 
     case request@SearchRequest(msg) =>
       println("Obtained: " + request)
       println("Sending to search actors")
+      val sw1 = internal.actorOf(Props(SearchActor(1)))
+      val sw2 = internal.actorOf(Props(SearchActor(2)))
       sw1 ! request
       sw2 ! request
 
